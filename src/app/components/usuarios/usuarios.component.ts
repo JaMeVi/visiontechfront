ba3856093109e27bd2172cb53dc1bafcd46d7ed0
Santiago0403 <u20201b291@upc.edu.pe>
2024-06-20 10:20:00 -0500

    [usuarios] Agrega interceptor para errores

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -4,3 +4,4 @@
 // Commit 4 - Agrega validaciones al formulario
 // Commit 5 - Refactoriza la lógica del componente
 // Commit 6 - Mejora uso del observable
+// Commit 7 - Agrega interceptor para errores
