85ed678de6a32bc573dc8e72fd51b6a66a0c49e5
gpc2602 <119427605+gpc2602@users.noreply.github.com>
2025-06-23 22:16:21 -0500

    Delete src/app/models/incidente.ts

--- src/app/models/incidente.ts
+++
@@ -1,9 +0,0 @@
-import { Ruta } from "./ruta"
-
-export class Incidente{
-    idIncidente:number=0
-    tipo:string=""
-    gravedad:string=""
-    descripcion:string=""
-    ruta:Ruta=new Ruta()
-}
