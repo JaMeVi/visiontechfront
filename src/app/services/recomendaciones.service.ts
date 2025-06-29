8ed58307bd920c7e792c9188b4a1564d9761dbb7
gpc2602 <119427605+gpc2602@users.noreply.github.com>
2025-06-29 13:40:55 -0500

    Update recomendaciones.service.ts

--- src/app/services/recomendaciones.service.ts
+++ src/app/services/recomendaciones.service.ts
@@ -1,30 +1,49 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environments';
-import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 import { Recomendaciones } from '../models/recomendaciones';
-const base_url = environment.base;
+import { HttpClient } from '@angular/common/http';
+
+const base_url=environment.base
+
 @Injectable({
   providedIn: 'root'
 })
 export class RecomendacionesService {
 
-  private url = `${base_url}/recomendaciones`;
   private listaCambio = new Subject<Recomendaciones[]>();
+  private url=`${base_url}/recomendaciones`
+  constructor(private http:HttpClient) { }
 
-  constructor(private h: HttpClient) { }
+   list() {
+      return this.http.get<Recomendaciones[]>(`${this.url}/lista`);
+    }
+  
+      
+    insert(r: Recomendaciones) {
+        return this.http.post(`${this.url}/inserciones`, r)
+    }
+  
+    getList() { // para actualizar automático (looks like)
+      return this.listaCambio.asObservable();
+    }
+  
+      setList(listaNueva: Recomendaciones[]) {
+        this.listaCambio.next(listaNueva);
+    }
+  
+     listId(id: number) {
+         return this.http.get<Recomendaciones>(`${this.url}/${id}`)
+       }
+     
+           update(r: Recomendaciones) {
+         return this.http.put(this.url, r)
+       }
+       
+           deleteA(id: number){
+           return this.http.delete(`${this.url}/${id}`)
+         }
+       
 
-  list() {
-    return this.h.get<Recomendaciones[]>((this.url));
-  }
 
-  insert(r: Recomendaciones) {
-    return this.h.post((this.url), r)
-  }
-  getList() {
-    return this.listaCambio.asObservable();
-  }
-  setList(listaNueva: Recomendaciones[]) {
-    this.listaCambio.next(listaNueva);
-  }
 }
