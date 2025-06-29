6ca1e508963c3330a71f70fbb85d217a293c0511
gpc2602 <119427605+gpc2602@users.noreply.github.com>
2025-06-29 13:40:31 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,87 +1,51 @@
+import { InsertarEditarIncidentesComponent } from './components/incidentes/insertar-editar-incidentes/insertar-editar-incidentes.component';
+import { InsertarEditarCatmosfericasComponent } from './components/catmosfericas/insertar-editar-catmosfericas/insertar-editar-catmosfericas.component';
+import { CEmergencia } from './models/contactoemergencia';
 import { Routes } from '@angular/router';
 import { RolesComponent } from './components/roles/roles.component';
 import path from 'path';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
 import { RutasComponent } from './components/rutas/rutas.component';
-import { AgregarActualizarRutasComponent } from './components/rutas/agregar-actualizar-rutas/agregar-actualizar-rutas.component';
-import { AgregarActualizarUsuariosComponent } from './components/usuarios/agregar-actualizar-usuarios/agregar-actualizar-usuarios.component';
-import { AgregarActualizarRolesComponent } from './components/roles/agregar-actualizar-roles/agregar-actualizar-roles.component';
-import { RecomendacionesComponent } from './components/recomendaciones/recomendaciones.component';
-import { AgregarActualizarRecomendacionesComponent } from './components/recomendaciones/agregar-actualizar-recomendaciones/agregar-actualizar-recomendaciones.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { Incidente } from './models/incidentes';
-import { IncidentesComponent } from './components/incidentes/incidentes.component';
-import { InsertarEditarIncidentesComponent } from './components/incidentes/insertar-editar-incidentes/insertar-editar-incidentes.component';
 import { MetricasComponent } from './components/metricas/metricas.component';
+import { CatmosfericasComponent } from './components/catmosfericas/catmosfericas.component';
+import { IncidentesComponent } from './components/incidentes/incidentes.component';
+import { RecomendacionesComponent } from './components/recomendaciones/recomendaciones.component';
+import { ContactoEmergenciaComponent } from './components/contacto-emergencia/contacto-emergencia.component';
 import { InsertarEditarMetricasComponent } from './components/metricas/insertar-editar-metricas/insertar-editar-metricas.component';
+import { AgregarActualizarRutasComponent } from './components/rutas/agregar-actualizar-rutas/agregar-actualizar-rutas.component';
+import { Component } from '@angular/core';
+import { InsertarEditarRecomendacionesComponent } from './components/recomendaciones/insertar-editar-recomendaciones/insertar-editar-recomendaciones.component';
 
 export const routes: Routes = [
-    {path:'',redirectTo:'roles', pathMatch:'full'   },
-    {path:'menu', component:MenuComponent,},
-    {path:'roles',component:RolesComponent,
+    {path:'',redirectTo:'roles', pathMatch:'full'},
+    {path:'roles',component:RolesComponent},
+    {path:'usuarios',component:UsuariosComponent},
+    {path:'rutas',component:RutasComponent,
+        children:[
+            {path:'inserciones',component:AgregarActualizarRutasComponent},
+            {path:'ediciones/:id',component:AgregarActualizarRutasComponent},]},
+    {path:'metricas',component:MetricasComponent,  
+        children:[
+            {path:'inserciones',component:InsertarEditarMetricasComponent},
+            {path:'ediciones/:id',component:InsertarEditarMetricasComponent},]},
+    {path:'catmosferica',component:CatmosfericasComponent,
+        children:[
+            {path:'inserciones',component:InsertarEditarCatmosfericasComponent},
+            {path:'ediciones/:id',component:InsertarEditarCatmosfericasComponent},]},
+    {path:'incidentes',component:IncidentesComponent,
+        children:[
+            {path:'inserciones',component:InsertarEditarIncidentesComponent},
+            {path:'ediciones/:id',component:InsertarEditarIncidentesComponent},]} ,
+    {path:'recomendaciones',component:RecomendacionesComponent,
         children:[
             {
-                path:'nuevo', component:AgregarActualizarRolesComponent,
+                path:'inserciones', component:InsertarEditarRecomendacionesComponent
             },
             {
-                path:'ediciones/:id', component:AgregarActualizarRolesComponent
-            }
+                path:'ediciones/id',component:InsertarEditarRecomendacionesComponent
+            },
         ]
     },
-    {path:'usuarios',component:UsuariosComponent,
-        children:[
-            {
-                path:'nuevo', component:AgregarActualizarUsuariosComponent,
-            },
-            {
-                path:'ediciones/:id', component:AgregarActualizarUsuariosComponent
-            }
-        ]
-    },
-    {path:'rutas',component:RutasComponent, 
-        children:[
-            {
-                path:'nuevo',
-                component:AgregarActualizarRutasComponent,
-            },
-            {
-                path:'ediciones/:id',   
-                component:AgregarActualizarRutasComponent,
-            }
-        ]
-    },
-    {
-        path:'recomendaciones', component:RecomendacionesComponent,
-        children:[
-            {
-                path:'nuevo',component:AgregarActualizarRecomendacionesComponent,
-            },
-            {
-                path:'ediciones/:id', component:AgregarActualizarRecomendacionesComponent,
-            }
-        ]
-    },
-    {
-        path:'incidentes', component:IncidentesComponent,
-        children:[
-            {
-                path:'nuevo', component:InsertarEditarIncidentesComponent,
-            },
-            {
-                path:'ediciones/:id',component:InsertarEditarIncidentesComponent,
-            }
-        ]
-    },
-    {
-        path:'metricas', component:MetricasComponent,
-        children:[
-            {
-                path:'nuevo', component:InsertarEditarMetricasComponent,
-            },
-            {
-                path:'ediciones/:id', component:InsertarEditarMetricasComponent,
-            }
-        ]
-    }
+    {path:'contactoemergencia',component:ContactoEmergenciaComponent}  
 
 ];
