33b9569afdd69acb96ae888f2d1242ac8c502b03
gpc2602 <119427605+gpc2602@users.noreply.github.com>
2025-06-30 20:15:05 -0500

    Add files via upload

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -3,7 +3,7 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-import { RouterLink, RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -11,28 +11,11 @@ import { RouterLink, RouterModule } from '@angular/router';
     MatMenuModule,
     MatButtonModule, 
     MatToolbarModule,
-    MatIconModule, RouterModule],
+    MatIconModule, 
+    RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
-  isMenuOpen = false;
-
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
-    const navLinks = document.querySelector('.nav-links') as HTMLElement;
-    
-    if (this.isMenuOpen) {
-      navLinks.classList.add('active');
-    } else {
-      navLinks.classList.remove('active');
-    }
-  }
-
-  closeMobileMenu() {
-    this.isMenuOpen = false;
-    const navLinks = document.querySelector('.nav-links') as HTMLElement;
-    navLinks.classList.remove('active');
-  }
 
 }
