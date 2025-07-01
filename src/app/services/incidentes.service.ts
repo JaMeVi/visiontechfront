c807bf97cabbce37b0fc146cbe03349f1dda94ea
gpc2602 <119427605+gpc2602@users.noreply.github.com>
2025-06-30 20:16:55 -0500

    Add files via upload

--- src/app/services/incidentes.service.ts
+++ src/app/services/incidentes.service.ts
@@ -1,9 +1,8 @@
-import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environments';
-import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
-import { Incidente } from '../models/incidentes';
-
+import { Incidente } from './../models/incidentes';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environments';
 
 const base_url=environment.base
 
@@ -13,35 +12,38 @@ const base_url=environment.base
 export class IncidentesService {
 
   private url=`${base_url}/incidentes`;
-  private listaCambio = new Subject<Incidente[]>();
+   private listaCambio = new Subject<Incidente[]>();
+  
+   constructor(private http:HttpClient) { }
+   
+     list() {
+       return this.http.get<Incidente[]>(`${this.url}/lista`);
+     }
+   
+     insert(m: Incidente) {
+       return this.http.post(`${this.url}/inserciones`, m)
+     }
+   
+     getList() { // para actualizar automático (looks like)
+       return this.listaCambio.asObservable();
+     }
  
-  constructor(private http:HttpClient) { }
+        setList(listaNueva: Incidente[]) {
+       this.listaCambio.next(listaNueva);
+     }
+ 
+   listId(id: number) {
+     return this.http.get<Incidente>(`${this.url}/${id}`)
+   }
+ 
+       update(m: Incidente) {
+     return this.http.put(this.url, m)
+   }
+   
+       deleteA(id: number){
+       return this.http.delete(`${this.url}/${id}`)
+     }
   
-    list() {
-      return this.http.get<Incidente[]>(`${this.url}/lista`);
-    }
   
-    insert(i: Incidente) {
-      return this.http.post(`${this.url}/nuevo`, i)
-    }
-  
-    getList() { // para actualizar automático (looks like)
-      return this.listaCambio.asObservable();
-    }
-
-       setList(listaNueva: Incidente[]) {
-      this.listaCambio.next(listaNueva);
-    }
-
-  listId(id: number) {
-    return this.http.get<Incidente>(`${this.url}/${id}`)
-  }
-
-      update(i: Incidente) {
-    return this.http.put(this.url, i)
   }
   
-      deleteA(id: number){
-      return this.http.delete(`${this.url}/${id}`)
-    }
-}
