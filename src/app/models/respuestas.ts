d193c0029cedec2d92a701136c28acc25d97d8ca
gpc2602 <119427605+gpc2602@users.noreply.github.com>
2025-07-02 16:18:15 -0500

    Add files via upload

---
+++ src/app/models/respuestas.ts
@@ -0,0 +1,10 @@
+import { Temaforo } from "./temasforo";
+
+export class Respuesta{
+
+    idRespuesta:number=0
+    respuesta:string=""
+    fechaRespuesta:Date=new Date()
+    temaForo:Temaforo=new Temaforo()
+
+}
