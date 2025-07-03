123a27009c6ddea8161bbde1dda2eba8301929bb
gpc2602 <119427605+gpc2602@users.noreply.github.com>
2025-07-02 19:39:10 -0500

    Add files via upload

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,5 @@
 export const environment={
     production:false,
-    base:"http://localhost:8083"
+    base:"http://localhost:8083",
+    googleMapsApiKey:'AIzaSyBWpmyX524_y5OOqIJTGufy_Fr_Zum6IKQ'
 }
