7f41046eeced030d25c5ac70a848a3812bf21d2b
Santiago0403 <u20201b291@upc.edu.pe>
2025-07-06 17:28:08 -0500

    access-denied TS

--- src/app/components/access-denied/access-denied.component.ts
+++ src/app/components/access-denied/access-denied.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-access-denied',
-  imports: [],
   templateUrl: './access-denied.component.html',
-  styleUrl: './access-denied.component.css'
+  styleUrls: ['./access-denied.component.css']
 })
 export class AccessDeniedComponent {
-
+  
+  constructor(private router: Router) {}
+  
+  goHome() {
+    this.router.navigate(['/homes']);
+  }
 }
